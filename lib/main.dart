c223c3bf419527fc7019fc9f34b57ec79a3d3f34
Lisandris <lis170694@gmail.com>
2022-10-04 21:23:53 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:app_notificaciones/screens/home_screen.dart';
+import 'package:app_notificaciones/screens/message_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home'   : ( _ ) => HomeScreen(),
+        'message': ( _ ) => MessageScreen(),
+      },
+    );
+  }
+}
